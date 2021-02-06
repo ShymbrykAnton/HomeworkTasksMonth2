bbbb311639eb88a880719f14fdad6f4441f95f2a
Shymbryk <shymbryk@gmail.com>
2021-02-06 19:22:02 +0200

    LinkedList2 done)))

--- src/main/java/ilist/impl/LinkedList2.java
+++ src/main/java/ilist/impl/LinkedList2.java
@@ -2,64 +2,239 @@ package ilist.impl;
 
 import ilist.IList;
 
+import java.util.Arrays;
+
+import static ilist.utils.Constants.*;
+
 public class LinkedList2 implements IList {
+    private Node root = null;
+    private Node tail = null;
+    private int size = 0;
+
+    static class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        public Node(Node prev, int value, Node next) {
+            this.value = value;
+            this.next = next;
+            this.prev = prev;
+        }
+
+    }
+
     @Override
     public void clear() {
-
+        root = null;
+        size = 0;
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public int get(int index) {
-        return 0;
+        if (isIndexCorrect(index) == -1) {
+            throw new IllegalArgumentException(INDEX_EXCEPTION);
+        }
+        boolean isStartFromRoot = index < size / 2 + size % 2;
+        return getNodeByIndex(isStartFromRoot ? root : tail, isStartFromRoot ? 0 : size - 1, index).value;
+    }
+
+    private Node getNodeByIndex(Node startElement, int startIndex, int index) {
+        if (startIndex == size / 2 + size % 2 && startIndex != index) {
+            throw new IllegalArgumentException(NO_NUMBER_IN_COLLECTION);
+        } else if (index == startIndex) {
+            return startElement;
+        } else if (index < size / 2 + size % 2) {
+            return getNodeByIndex(startElement.next, startIndex + 1, index);
+        } else {
+            return getNodeByIndex(startElement.prev, startIndex - 1, index);
+        }
     }
 
     @Override
     public boolean add(int value) {
-        return false;
+        Node newElement = new Node(null, value, null);
+        size++;
+        if (root == null) {
+            root = newElement;
+        } else {
+            newElement.prev = tail;
+            tail.next = newElement;
+        }
+        tail = newElement;
+        return true;
     }
 
     @Override
     public boolean add(int index, int value) {
-        return false;
+        if (index < 0 || index > size) {
+            return false;
+        }
+        if (root == null) {
+            add(value);
+            return true;
+        } else if (index == 0) {
+            Node newNode = new Node(null, value, root);
+            root.prev = newNode;
+            root = newNode;
+        } else if (index == size - 1) {
+            Node newNode = new Node(tail.prev, value, tail);
+            tail.prev.next = newNode;
+            tail = newNode.next;
+        } else {
+            boolean isStartFromRoot = index < size / 2 + size % 2;
+            Node ourNode = getNodeByIndex(isStartFromRoot ? root : tail, isStartFromRoot ? 0 : size - 1, index);
+            Node newNode = new Node(ourNode.prev, value, ourNode);
+            ourNode.prev.next = newNode;
+            ourNode.prev = newNode;
+        }
+        size++;
+        return true;
     }
 
     @Override
     public int remove(int number) {
-        return 0;
+        removeByIndex(getIndexByValue(root, tail, 0, size - 1, number));
+        return number;
     }
 
     @Override
     public int removeByIndex(int index) {
-        return 0;
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException(INDEX_EXCEPTION);
+        }
+        int ourValue;
+        if (root.next == null) {
+            ourValue = root.value;
+            root = null;
+        } else if (index == 0) {
+            ourValue = root.value;
+            root = root.next;
+            root.prev = null;
+        } else if (index == size - 1) {
+            ourValue = tail.value;
+            tail = tail.prev;
+            tail.next = null;
+        } else {
+            boolean isStartFromRoot = index < size / 2 + size % 2;
+            Node ourNode = getNodeByIndex(isStartFromRoot ? root : tail, isStartFromRoot ? 0 : size - 1, index);
+            ourValue = ourNode.value;
+            ourNode.next.prev = ourNode.prev;
+            ourNode.prev.next = ourNode.next;
+        }
+        size--;
+        return ourValue;
     }
 
     @Override
     public boolean contains(int value) {
-        return false;
+        return root != null && getIndexByValue(root, tail, 0, size - 1, value) >= 0;
+    }
+
+    private int getIndexByValue(Node startElement, Node endElement, int startIndex, int endIndex, int value) {
+        if (root!=null&&root.value == value) {
+            return startIndex;
+        }
+        if (startIndex == size / 2 + size % 2) {
+            return -1;
+        }
+        if (startElement.value == value) {
+            return startIndex;
+        }
+        if (endElement.value == value) {
+            return endIndex;
+        }
+        return getIndexByValue(startElement.next, endElement.prev, startIndex + 1, endIndex - 1, value);
     }
 
     @Override
     public boolean set(int index, int value) {
-        return false;
+        if (isIndexCorrect(index) == -1 || root == null) {
+            return false;
+        }
+        boolean isStartFromRoot = index < size / 2 + size % 2;
+        set(isStartFromRoot ? root : tail, isStartFromRoot ? 0 : size - 1, index, value);
+        return true;
+    }
+
+    private void set(Node startElement, int startIndex, int index, int value) {
+        if (startIndex == index) {
+            startElement.value = value;
+            return;
+        }
+        if (index < size / 2 + size % 2) {
+            set(startElement.next, startIndex + 1, index, value);
+        } else {
+            set(startElement.prev, startIndex - 1, index, value);
+        }
     }
 
     @Override
     public void print() {
+        if (size == 0) {
+            System.out.println(EMPTY_ARRAY);
+            return;
+        }
+        System.out.println(toString());
+    }
 
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder(LEFT_BRACKET);
+        int[] array = toArray();
+        for (int value : array) {
+            if (value == array[array.length - 1]) {
+                result.append(value);
+                break;
+            }
+            result.append(value).append(COMMA_AND_SPACE);
+        }
+        result.append(RIGHT_BRACKET);
+        return result.toString();
     }
 
     @Override
     public int[] toArray() {
-        return new int[0];
+        if (root == null) {
+            return new int[]{};
+        }
+        int[] array = new int[size()];
+        return toArray(root, 0, array);
+    }
+
+    private int[] toArray(Node element, int currentIndex, int[] array) {
+        if (currentIndex == size - 1) {
+            array[currentIndex] = element.value;
+            return array;
+        }
+        array[currentIndex] = element.value;
+        return toArray(element.next, currentIndex + 1, array);
     }
 
     @Override
     public boolean removeAll(int[] array) {
-        return false;
+        if (array == null || Arrays.equals(array, new int[]{})) {
+            return true;
+        }
+        boolean flag = false;
+        for (int value : array) {
+            if (contains(value)) {
+                remove(value);
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    private int isIndexCorrect(int index) {
+        if (index >= size || index < 0) {
+            return -1;
+        }
+        return index;
     }
 }
--- src/test/java/ilist/impl/LinkedList2Test.java
+++ src/test/java/ilist/impl/LinkedList2Test.java
@@ -1,56 +1,313 @@
 package ilist.impl;
 
+import ilist.IList;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class LinkedList2Test {
+    private static final IList collection0 = new LinkedList2();
+    private static final IList collection1 = new LinkedList2();
+    private static final IList collection9 = new LinkedList2();
+    private static final IList collection10 = new LinkedList2();
+    private static IList collection12 = new LinkedList2();
 
-    @Test
-    void clear() {
+    @BeforeEach
+    private void initializeCollection() {
+        collection1.add(1);
+
+        collection9.add(1);
+        collection9.add(2);
+        collection9.add(3);
+        collection9.add(4);
+        collection9.add(5);
+        collection9.add(6);
+        collection9.add(7);
+        collection9.add(8);
+        collection9.add(9);
+
+        collection10.add(1);
+        collection10.add(2);
+        collection10.add(3);
+        collection10.add(4);
+        collection10.add(5);
+        collection10.add(6);
+        collection10.add(7);
+        collection10.add(8);
+        collection10.add(9);
+        collection10.add(10);
+
+        collection12.add(1);
+        collection12.add(2);
+        collection12.add(3);
+        collection12.add(4);
+        collection12.add(5);
+        collection12.add(6);
+        collection12.add(7);
+        collection12.add(8);
+        collection12.add(9);
+        collection12.add(10);
+        collection12.add(11);
+        collection12.add(12);
+    }
+
+    @AfterEach
+    private void clearCollection() {
+        collection0.clear();
+        collection1.clear();
+        collection9.clear();
+        collection10.clear();
+        collection12.clear();
+    }
+
+    static Stream<Arguments> clearTest_Nominal() {
+        return Stream.of(
+                arguments(collection0),
+                arguments(collection1),
+                arguments(collection9),
+                arguments(collection10),
+                arguments(collection12)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("clearTest_Nominal")
+    void clearTest(IList collection) {
+        collection.clear();
+        int actual = collection.size();
+        Assertions.assertEquals(0, actual);
+    }
+
+    static Stream<Arguments> sizeTest_Nominal() {
+        return Stream.of(
+                arguments(0, collection0),
+                arguments(1, collection1),
+                arguments(9, collection9),
+                arguments(10, collection10),
+                arguments(12, collection12)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("sizeTest_Nominal")
+    void sizeTest(int expected, IList collection) {
+        int actual = collection.size();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> getTest_Nominal() {
+        return Stream.of(
+                arguments(1, collection1, 0),
+                arguments(2, collection9, 1),
+                arguments(10, collection10, 9),
+                arguments(7, collection12, 6)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("getTest_Nominal")
+    void getTest(int expected, IList collection, int index) {
+        int actual = collection.get(index);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> addTest_Nominal() {
+        return Stream.of(
+                arguments(1, collection0, new int[]{1}),
+                arguments(5, collection1, new int[]{1, 5}),
+                arguments(2, collection9, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 2}),
+                arguments(46, collection10, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 46}),
+                arguments(13, collection12, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("addTest_Nominal")
+    void addValueTest(int value, IList collection, int[] expected1) {
+        collection.add(value);
+        int[] actual1 = collection.toArray();
+        Assertions.assertArrayEquals(expected1, actual1);
+    }
+
+    static Stream<Arguments> addValueByIndexTest_Nominal() {
+        return Stream.of(
+                arguments(false, 1, collection0, -5, new int[]{}),
+                arguments(false, 1, collection9, -5, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}),
+                arguments(true, 15, collection0, 0, new int[]{15}),
+                arguments(true, 5, collection1, 0, new int[]{5, 1}),
+                arguments(true, 10, collection9, 5, new int[]{1, 2, 3, 4, 5, 10, 6, 7, 8, 9}),
+                arguments(true, 46, collection10, 9, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 46, 10}),
+                arguments(true, 13, collection12, 7, new int[]{1, 2, 3, 4, 5, 6, 7, 13, 8, 9, 10, 11, 12})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("addValueByIndexTest_Nominal")
+    void addValueByIndexTest(boolean expected, int value, IList collection, int index, int[] expected1) {
+        boolean actual = collection.add(index, value);
+        int[] actual1 = collection.toArray();
+        Assertions.assertEquals(expected, actual);
+        Assertions.assertArrayEquals(expected1, actual1);
+    }
+
+    static Stream<Arguments> removeTest_Nominal() {
+        if (!Arrays.equals(collection12.toArray(), new int[]{1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12})) {
+            collection12 = new ArrayList1(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12});
+        }
+        return Stream.of(
+                arguments(1, collection1, 1, new int[]{}),
+                arguments(1, collection9, 1, new int[]{2, 3, 4, 5, 6, 7, 8, 9}),
+                arguments(5, collection9, 5, new int[]{1, 2, 3, 4, 6, 7, 8, 9}),
+                arguments(9, collection10, 9, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 10}),
+                arguments(10, collection10, 10, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}),
+                arguments(5, collection12, 5, new int[]{1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 12})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("removeTest_Nominal")
+    void removeTest(int number, IList collection, int expected, int[] expected1) {
+        int actual = collection.remove(number);
+        int[] actual1 = collection.toArray();
+        Assertions.assertEquals(expected, actual);
+        Assertions.assertArrayEquals(expected1, actual1);
+    }
+
+    static Stream<Arguments> removeByIndexTest_Nominal() {
+        return Stream.of(
+                arguments(1, collection1, 0, new int[]{}),
+                arguments(1, collection9, 0, new int[]{2, 3, 4, 5, 6, 7, 8, 9}),
+                arguments(9, collection9, 8, new int[]{1, 2, 3, 4, 5, 6, 7, 8}),
+                arguments(6, collection9, 5, new int[]{1, 2, 3, 4, 5, 7, 8, 9}),
+                arguments(10, collection10, 9, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}),
+                arguments(8, collection12, 7, new int[]{1, 2, 3, 4, 5, 6, 7, 9, 10, 11, 12})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("removeByIndexTest_Nominal")
+    void removeByIndexTest(int expected, IList collection, int index, int[] expected1) {
+        int actual = collection.removeByIndex(index);
+        int[] actual1 = collection.toArray();
+        Assertions.assertEquals(expected, actual);
+        Assertions.assertArrayEquals(expected1, actual1);
+    }
+
+    static Stream<Arguments> containsTest_Nominal() {
+        return Stream.of(
+                arguments(false, collection0, 0),
+                arguments(false, collection1, 0),
+                arguments(true, collection1, 1),
+                arguments(true, collection9, 5),
+                arguments(true, collection10, 10),
+                arguments(false, collection12, 78)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("containsTest_Nominal")
+    void containsTest(boolean expected, IList collection, int value) {
+        boolean actual = collection.contains(value);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    static Stream<Arguments> setTest_Nominal() {
+        return Stream.of(
+                arguments(false, collection0, 0, 5),
+                arguments(false, collection1, -5, 10),
+                arguments(true, collection9, 5, 30),
+                arguments(true, collection9, 3, 30),
+                arguments(true, collection10, 9, 50),
+                arguments(false, collection12, -55, 32)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("setTest_Nominal")
+    void setTest(boolean expected, IList collection, int index, int value) {
+        boolean actual = collection.set(index, value);
+        Assertions.assertEquals(expected, actual);
     }
 
     @Test
-    void size() {
+    void printTest() {
+        collection0.print();
+        collection1.print();
+        collection9.print();
+        collection10.print();
+        collection12.print();
     }
 
-    @Test
-    void get() {
+    //
+    static Stream<Arguments> toArrayTest_Nominal() {
+        return Stream.of(
+                arguments(collection0, new int[]{}),
+                arguments(collection1, new int[]{1}),
+                arguments(collection9, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}),
+                arguments(collection10, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}),
+                arguments(collection12, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12})
+        );
     }
 
-    @Test
-    void add() {
+    @ParameterizedTest
+    @MethodSource("toArrayTest_Nominal")
+    void toArrayTest(IList collection, int[] expected) {
+        int[] actual = collection.toArray();
+        Assertions.assertArrayEquals(expected, actual);
     }
 
-    @Test
-    void testAdd() {
+    static Stream<Arguments> removeAllTest_Nominal() {
+        return Stream.of(
+//                arguments(true, collection1, null, new int[]{1}),
+//                arguments(true, collection9, new int[]{}, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9}),
+//                arguments(true, collection1, new int[]{1}, new int[]{}),
+//                arguments(true, collection1, new int[]{1}, new int[]{}),
+//                arguments(true, collection9, new int[]{1, 4, 9}, new int[]{2, 3, 5, 6, 7, 8}),
+                arguments(true, collection9, new int[]{1, 4, 5}, new int[]{2, 3, 6, 7, 8, 9})
+//                arguments(true, collection10, new int[]{9, 123, 2}, new int[]{1, 3, 4, 5, 6, 7, 8, 10}),
+//                arguments(false, collection12, new int[]{100, -5, 13}, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12})
+        );
     }
 
-    @Test
-    void remove() {
+    @ParameterizedTest
+    @MethodSource("removeAllTest_Nominal")
+    void removeAllTest(boolean expected, IList collection, int[] ints, int[] expected1) {
+        boolean actual = collection.removeAll(ints);
+        int[] actual1 = collection.toArray();
+        System.out.println(Arrays.toString(actual1));
+        System.out.println(Arrays.toString(expected1));
+        Assertions.assertEquals(expected, actual);
+        Assertions.assertArrayEquals(expected1, actual1);
     }
 
-    @Test
-    void removeByIndex() {
+
+    static Stream<Arguments> ExceptionTest_Nominal() {
+        Executable executable;
+        return Stream.of(
+                arguments(executable = () -> collection0.removeByIndex(2)),
+                arguments(executable = () -> collection12.removeByIndex(-5)),
+                arguments(executable = () -> collection9.removeByIndex(10)),
+                arguments(executable = () -> collection0.remove(2)),
+                arguments(executable = () -> collection9.remove(23)),
+                arguments(executable = () -> collection9.get(10)),
+                arguments(executable = () -> collection9.get(-5))
+        );
     }
 
-    @Test
-    void contains() {
-    }
-
-    @Test
-    void set() {
-    }
-
-    @Test
-    void print() {
-    }
-
-    @Test
-    void toArray() {
-    }
-
-    @Test
-    void removeAll() {
+    @ParameterizedTest
+    @MethodSource("ExceptionTest_Nominal")
+    void getException1Test(Executable executable) throws IllegalArgumentException {
+        Throwable thrown = Assertions.assertThrows(IllegalArgumentException.class, executable);
+        Assertions.assertNotNull(thrown.getMessage());
     }
 }
